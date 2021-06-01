2840d10c660930ab72947c2f6c3bec6baca305fb
CUPZYY <richbanana1234@gmail.com>
2021-06-01 20:36:39 +0200

    Renamed cameraclip to cameratweaks and added distance options.

--- src/main/java/bleach/hack/mixin/MixinCamera.java
+++ src/main/java/bleach/hack/mixin/MixinCamera.java
@@ -8,7 +8,10 @@
  */
 package bleach.hack.mixin;
 
+import bleach.hack.module.Module;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -22,16 +25,35 @@ import net.minecraft.fluid.Fluids;
 @Mixin(Camera.class)
 public class MixinCamera {
 
+	@Unique
+	private boolean bypassCameraClip;
+
+	@Shadow
+	private double clipToSpace(double desiredCameraDistance) { return 0; }
+
 	@Inject(method = "getSubmergedFluidState", at = @At("HEAD"), cancellable = true)
 	private void getSubmergedFluidState(CallbackInfoReturnable<FluidState> cir) {
 		if (((NoRender) ModuleManager.getModule("NoRender")).shouldRemoveOverlay(3)) {
 			cir.setReturnValue(Fluids.EMPTY.getDefaultState());
 		}
 	}
+
 	@Inject(method = "clipToSpace", at = @At("HEAD"), cancellable = true)
 	private void onClipToSpace(double desiredCameraDistance, CallbackInfoReturnable<Double> info) {
-		if (ModuleManager.getModule("CameraClip").isEnabled()) {
-			info.setReturnValue(desiredCameraDistance);
+		if (bypassCameraClip) {
+			bypassCameraClip = false;
+		} else {
+			Module CameraTweaks = ModuleManager.getModule("CameraTweaks");
+
+			if (CameraTweaks.isEnabled()) {
+				if (CameraTweaks.getSetting(0).asToggle().state) {
+					info.setReturnValue(CameraTweaks.getSetting(1).asToggle().state
+							? CameraTweaks.getSetting(1).asToggle().getChild(0).asSlider().getValue() : desiredCameraDistance);
+				} else if (CameraTweaks.getSetting(1).asToggle().state) {
+					bypassCameraClip = true;
+					clipToSpace(desiredCameraDistance);
+				}
+			}
 		}
 	}
 }
