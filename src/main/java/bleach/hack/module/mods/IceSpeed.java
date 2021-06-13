96bc414873ba8205c35a1a99806e73e7f7215cf7
bufferfish <69718294+bleachhack@users.noreply.github.com>
2021-06-13 22:16:55 +0200

    Update IceSpeed.java

--- src/main/java/bleach/hack/module/mods/IceSpeed.java
+++ src/main/java/bleach/hack/module/mods/IceSpeed.java
@@ -12,28 +12,30 @@ import net.minecraft.block.Blocks;
 import java.util.Arrays;
 
 public class IceSpeed extends Module {
+
+    private static final Set<Block> ICE_BLOCKS = Sets.newHashSet(Blocks.ICE, Blocks.PACKED_ICE, Blocks.FROSTED_ICE, Blocks.BLUE_ICE);
+
     public IceSpeed() {
         super("IceSpeed", KEY_UNBOUND, Category.MOVEMENT, "Move faster on ice",
-        new SettingSlider("Speed", 0, 9, 4, 2).withDesc("The opacity of the fill"));
+                new SettingSlider("Speed", 0, 9, 4, 2).withDesc("How fast to move when on ice"));
     }
 
     @Subscribe
     public void onTick(EventTick event) {
-        setSlipperiness(((1) - (getSetting(0).asSlider().getValueFloat() / 10)));
+        setSlipperiness(1f - getSetting(0).asSlider().getValueFloat() / 10f);
     }
 
     @Override
     public void onDisable() {
-        setSlipperiness(0.98f);
-        for(Block block: Arrays.asList(Blocks.ICE, Blocks.PACKED_ICE, Blocks.FROSTED_ICE, Blocks.BLUE_ICE)) {
-            FabricReflect.writeField(block, 0.989f, "slipperiness", "field_23163");
-        }
+        setSlipperiness(0.989f);
         super.onDisable();
     }
 
-    private void setSlipperiness(float speed) {
-        for(Block block: Arrays.asList(Blocks.ICE, Blocks.PACKED_ICE, Blocks.FROSTED_ICE, Blocks.BLUE_ICE)){
-            FabricReflect.writeField(block, speed, "slipperiness", "field_23163");
+    private void setSlipperiness(float slipperiness) {
+        for (Block block: ICE_BLOCKS) {
+            if (block.getSlipperiness() != slipperiness) {
+                FabricReflect.writeField(block, slipperiness, "slipperiness", "field_23163");
+            }
         }
     }
 }
